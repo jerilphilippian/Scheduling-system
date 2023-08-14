de987d13d62e9285f4077dc06b7f532e36aead08
Jerremie08 <jerremiegellang.philippian@gmail.com>
2023-08-14 16:01:52 +0800

    Dashboard - Count Users and Rooms

--- app/Http/Livewire/Dashboard/Index.php
+++ app/Http/Livewire/Dashboard/Index.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Livewire\Dashboard;
 
+use App\Models\Room;
+use App\Models\User;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $activeUser;
+    public $inactiveUser;
+
+    public $totalRooms;
+
+    public function mount()
+    {
+        $this->activeUser = User::where('is_active', 1)->count();
+        $this->inactiveUser = User::where('is_active', 0)->count();
+
+        $this->totalRooms = Room::count();
+    }
+
     public function render()
     {
         return view('livewire.dashboard.index');
