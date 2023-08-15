2b91b7fed65024951f78b5c894eb4c0fcf70c2c7
Jerremie08 <jerremiegellang.philippian@gmail.com>
2023-08-15 19:44:44 +0800

    Invitations display function added

--- app/Http/Livewire/Events/Invitation/Index.php
+++ app/Http/Livewire/Events/Invitation/Index.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Events\Invitation;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $user;
+
     public function render()
     {
+        $this->user = Auth::user();
         return view('livewire.events.invitation.index');
     }
 }
